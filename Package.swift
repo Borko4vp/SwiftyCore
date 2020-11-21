104a2a35acb49e3e50f7ba8940ce309f7f295980
Borko Tomić <borko.tomic@avawomen.com>
2020-11-21 13:26:29 +0100

    Adding zip foundation

--- Package.swift
+++ Package.swift
@@ -17,14 +17,15 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         //.package(url: "https://github.com/airbnb/lottie-ios.git", .upToNextMajor(from: "3.1.9"))
-        .package(url: "https://github.com/NordicSemiconductor/IOS-Pods-DFU-Library", .upToNextMajor(from: "4.8.0"))
+        //.package(url: "https://github.com/NordicSemiconductor/IOS-Pods-DFU-Library", .upToNextMajor(from: "4.8.0"))
+        .package(url: "https://github.com/weichsel/ZIPFoundation", .exact("0.9.11"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftyCore",
-            dependencies: ["NordicDFU"]),
+            dependencies: ["ZIPFoundation"]),
         .testTarget(
             name: "SwiftyCoreTests",
             dependencies: ["SwiftyCore"]),
