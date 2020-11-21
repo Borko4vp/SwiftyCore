cffd857d002a57a2932d2609004adee3d2abdd9e
Borko Tomić <borko.tomic@avawomen.com>
2020-11-21 11:42:31 +0100

    Lottie dependecie trying more

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftyCore",
-            dependencies: ["lottie-ios"]),
+            dependencies: ["Lottie"]),
         .testTarget(
             name: "SwiftyCoreTests",
             dependencies: ["SwiftyCore"]),
