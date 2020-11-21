ba214d72696c061f77288b7efa25793be76b5785
Borko Tomić <borko.tomic@avawomen.com>
2020-11-21 13:08:15 +0100

    one more

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .target(
             name: "SwiftyCore",
             dependencies: [
-                .product(name: "Lottie", package: "lottie-ios")
+                .product(name: "lottie-ios", package: "lottie-ios")
             ]),
         .testTarget(
             name: "SwiftyCoreTests",
