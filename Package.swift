77ef06ebb27e69389ecd8c2c37a7dd63d1dd953f
Borko Tomić <borko.tomic@avawomen.com>
2020-11-21 12:59:02 +0100

    Addic nordicDFU dependency

--- Package.swift
+++ Package.swift
@@ -16,14 +16,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/airbnb/lottie-ios.git", .upToNextMajor(from: "3.1.9"))
+        //.package(url: "https://github.com/airbnb/lottie-ios.git", .upToNextMajor(from: "3.1.9")),
+        .package(url: "https://github.com/NordicSemiconductor/IOS-Pods-DFU-Library", .upToNextMajor(from: "4.8.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftyCore",
-            dependencies: ["lottie-ios"]),
+            dependencies: ["NordicDFU"]),
         .testTarget(
             name: "SwiftyCoreTests",
             dependencies: ["SwiftyCore"]),
