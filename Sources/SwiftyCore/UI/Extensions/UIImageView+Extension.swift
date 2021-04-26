e71c67adfb3cb9055a9e0119e2fd0c48e7099549
Borko Tomić <borko.tomic@avawomen.com>
2021-04-26 15:03:59 +0200

    Adjusting remote image settings to support dataTask persisitng, and custom actions with it

--- Sources/SwiftyCore/UI/Extensions/UIImageView+Extension.swift
+++ Sources/SwiftyCore/UI/Extensions/UIImageView+Extension.swift
@@ -9,45 +9,69 @@ import Foundation
 import UIKit
 
 public extension UIImageView {
+    func getRemoteFetchDataTaskAndSetImage(from url: String?, headers: [String: String]? = nil, placeholderImage: UIImage? = nil) -> URLSessionDataTask? {
+        DispatchQueue.main.async() { [weak self] in
+            self?.image = placeholderImage
+        }
+        guard let urlString = url, let imageUrl = URL(string: urlString) else { return nil }
+        let dataTask = getImageDataTask(from: imageUrl, headers: headers) { [weak self] remoteImage in
+            guard let remoteImage = remoteImage else { return }
+            DispatchQueue.main.async() {
+                self?.image = remoteImage
+            }
+        }
+        return dataTask
+    }
+    
     func setRemoteImage(from url: String?, headers: [String: String]? = nil, placeholderImage: UIImage? = nil) {
         DispatchQueue.main.async() { [weak self] in
             self?.image = placeholderImage
         }
         guard let urlString = url, let imageUrl = URL(string: urlString) else { return }
-        getImage(from: imageUrl, headers: headers) { remoteImage in
+        getImageDataTask(from: imageUrl, headers: headers) { [weak self] remoteImage in
             guard let remoteImage = remoteImage else { return }
-            DispatchQueue.main.async() { [weak self] in
+            DispatchQueue.main.async() {
                 self?.image = remoteImage
             }
         }
     }
     
-    private func getImage(from url: URL, headers: [String: String]? = nil, completion: @escaping (UIImage?) -> Void) {
+    @discardableResult
+    private func getImageDataTask(from url: URL, headers: [String: String]? = nil, completion: @escaping (UIImage?) -> Void) -> URLSessionDataTask? {
         if let cachedImage = SwiftyCore.UI.ImageCache.shared.get(url: url.absoluteString) {
             DispatchQueue.main.async() {
                 completion(cachedImage)
             }
-            return
+            return nil
         } else {
             var urlRequest = URLRequest(url: url)
             urlRequest.allHTTPHeaderFields = headers
-            URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-                guard
-                    let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                    //let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                    let data = data, error == nil,
-                    let image = UIImage(data: data)
-                    else {
-                    DispatchQueue.main.async() {
-                        completion(nil)
-                    }
-                    return
-                }
-                SwiftyCore.UI.ImageCache.shared.add(url: url.absoluteString, image: image)
-                DispatchQueue.main.async() {
-                    completion(image)
-                }
-            }.resume()
+            let dataTask = self.createImageDownloadTask(from: url, with: headers, completion: completion)
+            dataTask.resume()
+            return dataTask
         }
+    }
+    
+    private func createImageDownloadTask(from url: URL, with headers: [String: String]? = nil, completion: @escaping (UIImage?) -> Void) -> URLSessionDataTask {
+        var urlRequest = URLRequest(url: url)
+        urlRequest.allHTTPHeaderFields = headers
+        let dataTask = URLSession.shared.dataTask(with: urlRequest,completionHandler: { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                //let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else {
+                DispatchQueue.main.async() {
+                    completion(nil)
+                }
+                return
+            }
+            SwiftyCore.UI.ImageCache.shared.add(url: url.absoluteString, image: image)
+            DispatchQueue.main.async() {
+                completion(image)
+            }
+        })
+        return dataTask
     }
 }
