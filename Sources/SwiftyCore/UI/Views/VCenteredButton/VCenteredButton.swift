9bdec727ff1bbed0740ff4d356fd3bcf3bbffba5
Borko Tomić <borko.tomic@avawomen.com>
2021-04-30 17:15:21 +0200

    Adjusting VCentered button title and image rects

--- Sources/SwiftyCore/UI/Views/VCenteredButton/VCenteredButton.swift
+++ Sources/SwiftyCore/UI/Views/VCenteredButton/VCenteredButton.swift
@@ -10,26 +10,21 @@ import UIKit
 public class VCenteredButton: UIButton {
     public var imageAboveTitle: Bool = true
     
-    var spacing: CGFloat {
-        return self.bounds.height/6
-    }
+    var spacing: CGFloat { 8 }
+
     
-    var padding: CGFloat = 4
+    var padding: CGFloat { 4 }
     
     public override func titleRect(forContentRect contentRect: CGRect) -> CGRect {
-        let rect = super.titleRect(forContentRect: contentRect)
-        
-        let yOrigin = imageAboveTitle ? contentRect.height - rect.height - spacing : spacing
-        return CGRect(x: padding, y: yOrigin,
-                      width: contentRect.width - 2*padding, height: rect.height)
+        let yOrigin = imageAboveTitle ? bounds.height/2 + 2*spacing : spacing
+        return CGRect(x: spacing, y: yOrigin,
+                      width: contentRect.width - 2*spacing, height: bounds.height/2 - 3*spacing)
     }
     
     public override func imageRect(forContentRect contentRect: CGRect) -> CGRect {
-        let rect = super.imageRect(forContentRect: contentRect)
-        let imageAspect = rect.height/rect.width
         let titleRect = self.titleRect(forContentRect: contentRect)
-        let imageHeight = (contentRect.height - titleRect.height) - 3*spacing
-        let imageWidth = imageHeight/imageAspect
+        let imageHeight = bounds.height/2
+        let imageWidth = contentRect.width - 4*spacing
         let yOrigin = imageAboveTitle ? spacing : titleRect.height + 2*spacing
         return CGRect(x: contentRect.width/2.0 - imageWidth/2.0,
                       y: yOrigin,
@@ -61,5 +56,7 @@ public class VCenteredButton: UIButton {
     
     private func centerTitleLabel() {
         self.titleLabel?.textAlignment = .center
+        self.titleLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
+        self.imageView?.contentMode = .scaleAspectFit
     }
 }
